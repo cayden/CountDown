fa4437b7646c59d288f563dbbc73ef86ea937849
cuiran <cuiran2001@163.com>
2014-12-19 14:12:37 +0800

    添加一个自定义的MySurfaceView

--- src/com/cayden/countdown/MainActivity.java
+++ src/com/cayden/countdown/MainActivity.java
@@ -1,15 +1,27 @@
 package com.cayden.countdown;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.cayden.countdown.view.MySurfaceView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		 // 隐藏状态栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 把Activity的标题去掉
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 设置布局
+        setContentView(new MySurfaceView(this));
 	}
 
 	@Override
